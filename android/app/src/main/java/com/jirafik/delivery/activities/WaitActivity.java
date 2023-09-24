9fa58c3bebf28f461b2f31bbe3c0e59184964611
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-09-24 11:56:47 +0300

    added few activities on auth level (design)

--- android/app/src/main/java/com/jirafik/delivery/activities/WaitActivity.java
+++ android/app/src/main/java/com/jirafik/delivery/activities/WaitActivity.java
@@ -1,15 +1,16 @@
-package com.jirafik.delivery;
+package com.jirafik.delivery.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.jirafik.delivery.R;
+
+public class WaitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wait);
     }
-
 }
