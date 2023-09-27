a714ea04d75915cf573a8a502aae6348ebcfb3e8
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-09-27 16:24:36 +0300

    configured wait activity. created main activity. Trying to make it look nice

--- android/app/src/main/java/com/jirafik/delivery/activities/WaitActivity.java
+++ android/app/src/main/java/com/jirafik/delivery/activities/WaitActivity.java
@@ -2,6 +2,7 @@ package com.jirafik.delivery.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.jirafik.delivery.R;
@@ -12,5 +13,15 @@ public class WaitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait);
+
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(WaitActivity.this, MainPageActivity.class));
+                    }
+                },
+                5000
+        );
     }
 }
