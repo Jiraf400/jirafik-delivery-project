18115177de9f6a21d3d226e4c5d82cb4faf3957c
Jiraf400 <109845546+Jiraf400@users.noreply.github.com>
2023-10-14 18:19:37 +0300

    add spring cloud gateway

---
+++ backend/GatewayServer/src/test/java/com/jirafik/gate/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jirafik.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
